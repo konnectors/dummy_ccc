b32a8284997c417d4b3fb5513da3339c55fc5f70
LucsT <lucas@cozycloud.cc>
2023-07-26 18:36:48 +0200

    publish: fix: Add getUserDataFromWebsite
    generated from commit e9811877015da522103268dad7aad847e504a887

--- main.js
+++ main.js
@@ -1312,8 +1312,9 @@ var ContentScript = /*#__PURE__*/function () {
       }
 
       var now = new Date().toISOString();
-      var timestamp = now + '-' + LOG_NUMBER.padStart(4, '0');
-      LOG_NUMBER = LOG_NUMBER + 1;
+      var timestamp = now; //+ '-' + LOG_NUMBER.padStart(4, '0')
+      //LOG_NUMBER = LOG_NUMBER + 1
+
       (_this$bridge2 = this.bridge) === null || _this$bridge2 === void 0 ? void 0 : _this$bridge2.emit('log', {
         timestamp: timestamp,
         level: newLevel,
